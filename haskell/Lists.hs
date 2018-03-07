6b3164753495a479cd1fea9f70a92924fcd45a3c
Tanner <tanner.kvarfordt@aggiemail.usu.edu>
2018-03-06 21:11:18 -0700

    Fixed indentation... again. It's right this time I swear.

--- haskell/Lists.hs
+++ haskell/Lists.hs
@@ -1,13 +1,13 @@
 module Lists where
 
-    -- I've left this sample definition in here, you should delete it
-    oddNumbers :: Int -> [Int]
-    oddNumbers n = [ x | x <- [1..n], (mod x 1) == 0] 
+-- I've left this sample definition in here, you should delete it
+oddNumbers :: Int -> [Int]
+oddNumbers n = [ x | x <- [1..n], (mod x 1) == 0] 
 
-    -- TODO: document countingNumbers
-    countingNumbers :: [Integer]
-    countingNumbers = [1..]
+-- TODO: document countingNumbers
+countingNumbers :: [Integer]
+countingNumbers = [1..]
 
-    -- TODO: document evenNumbers
-    evenNumbers :: [Integer]
-    evenNumbers = [x * 2 | x <- countingNumbers]
+-- TODO: document evenNumbers
+evenNumbers :: [Integer]
+evenNumbers = [x * 2 | x <- countingNumbers]
