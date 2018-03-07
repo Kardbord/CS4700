0fb95404fcc070981f30de1cf700136202a70036
Tanner <tanner.kvarfordt@aggiemail.usu.edu>
2018-03-06 18:05:41 -0700

    Implemented countingNumbers function

--- haskell/Lists.hs
+++ haskell/Lists.hs
@@ -3,3 +3,6 @@ module Lists where
     -- I've left this sample definition in here, you should delete it
     oddNumbers :: Int -> [Int]
     oddNumbers n = [ x | x <- [1..n], (mod x 1) == 0] 
+
+    -- TODO: document countingNumbers
+    countingNumbers = [1..]
