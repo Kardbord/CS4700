370d9a04d3933678d9d3b9874da0b2950ae67943
Tanner <tanner.kvarfordt@aggiemail.usu.edu>
2018-03-06 17:35:34 -0700

    Fixed indendation in haskell files

--- haskell/Lists.hs
+++ haskell/Lists.hs
@@ -1,5 +1,5 @@
 module Lists where
 
-  -- I've left this sample definition in here, you should delete it
-  oddNumbers :: Int -> [Int]
-  oddNumbers n = [ x | x <- [1..n], (mod x 1) == 0] 
+    -- I've left this sample definition in here, you should delete it
+    oddNumbers :: Int -> [Int]
+    oddNumbers n = [ x | x <- [1..n], (mod x 1) == 0] 
