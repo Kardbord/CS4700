a042b920c0af96484b32840c032059318a084c69
Tanner <tanner.kvarfordt@aggiemail.usu.edu>
2018-03-06 18:22:14 -0700

    added type signatures to the implemented functions

--- haskell/Lists.hs
+++ haskell/Lists.hs
@@ -5,7 +5,9 @@ module Lists where
     oddNumbers n = [ x | x <- [1..n], (mod x 1) == 0] 
 
     -- TODO: document countingNumbers
+    countingNumbers :: [Integer]
     countingNumbers = [1..]
 
     -- TODO: document evenNumbers
+    evenNumbers :: [Integer]
     evenNumbers = [x * 2 | x <- countingNumbers]
