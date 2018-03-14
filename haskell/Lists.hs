76d79c8f312d9f6c557f6c3c5cea44db405f5ef5
Tanner <tanner.kvarfordt@aggiemail.usu.edu>
2018-03-14 16:47:18 -0600

    Added some documentation

--- haskell/Lists.hs
+++ haskell/Lists.hs
@@ -5,22 +5,32 @@ module Lists where
 oddNumbers :: Int -> [Int]
 oddNumbers n = [ x | x <- [1..n], (mod x 1) == 0] 
 
--- Provides an infinite list of whole numbers beginning at 1
+-- Provides an unbounded list of whole numbers beginning at 1
+--
+-- return : an unbounded list of positive integers
 countingNumbers :: [Integer]
 countingNumbers = [1..]
 
--- Provides an infinite list of even numbers beginning at 2
+-- Provides an unbounded list of even numbers beginning at 2
+--
+-- return : an unbounded list of positive even numbers
 evenNumbers :: [Integer]
 evenNumbers = [x * 2 | x <- countingNumbers]
 
--- Provides an infinite list of prime numbers
+-- Provides an unbounded list of prime numbers
 -- Primes are computed using Wilson's Theorem
 -- https://en.wikipedia.org/wiki/Wilson%27s_theorem#Example
+--
+-- return : an unbounded list of prime numbers
 primeNumbers :: [Integer]
 primeNumbers = [x | x <- [2..], ((product [1..x-1] + 1) `mod` x == 0)]
 
 -- Merges two lists of Integers together
 -- Assumes the two lists are already sorted
+--
+-- param xs1 : a sorted list
+-- param xs2 : a sorted list
+-- return    : @xs1 and @xs2 merged into one sorted list
 merge :: [Integer] -> [Integer] -> [Integer]
 merge xs1 [] = xs1
 merge [] xs2 = xs2
@@ -28,3 +38,4 @@ merge xs1 xs2 =
     if head xs1 <= head xs2
         then (head xs1) : (merge (tail xs1) xs2)
         else (head xs2) : (merge (tail xs2) xs1)
+
