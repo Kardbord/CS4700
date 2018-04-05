a9ce0ceacf65358e83ffe8a54da8b5222b7c5196
Tanner <tanner.kvarfordt@aggiemail.usu.edu>
2018-04-05 09:30:54 -0600

    Fixed typos in try left and try right

--- prolog/solve.pl
+++ prolog/solve.pl
@@ -3,9 +3,9 @@ try(Row, Column, NextRow, NextColumn) :- NextRow is Row, NextColumn is Column - 
 % Try a move in the "down" direction, assumes Row and Column are bound.
 try(Row, Column, NextRow, NextColumn) :- NextRow is Row, NextColumn is Column + 1.
 % Try a move in the "right" direction, assumes Row and Column are bound.
-try(Row, Column, NextRow, NextColumn) :- NextRow is Row + 1, NexColumn is Column.
+try(Row, Column, NextRow, NextColumn) :- NextRow is Row + 1, NextColumn is Column.
 % Try a move in the "left" direction, assumes Row and Column are bound.
-try(Row, Column, NextRow, NextColumn) :- NextRow is Row - 1, NexColumn is Column.
+try(Row, Column, NextRow, NextColumn) :- NextRow is Row - 1, NextColumn is Column.
 
 % move(Maze, List, NewList, Row, Column, GoalRow, GoalColumn) - moves, 
 %   and keep on moving until the GoalRow and GoalColumn coordinates 
@@ -24,7 +24,7 @@ printCell(Maze, _, Row, Column) :- maze(Maze, Row, Column, barrier), write('x').
 %   Print an open cell.
 printCell(Maze, _, Row, Column) :- maze(Maze, Row, Column, open), write(' ').
 
-printMaze(Maze, List) :- true.
+printMaze(Maze, List) :- true. 
 
 solve(Maze) :- true.
 
