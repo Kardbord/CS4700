80d65443b4803d034bfdce51e1f968365047b7f3
Tanner <tanner.kvarfordt@aggiemail.usu.edu>
2018-04-06 13:29:58 -0600

    Got printRow working

--- prolog/solve.pl
+++ prolog/solve.pl
@@ -31,19 +31,26 @@ try(Row, Column, NextRow, NextColumn) :- NextRow is Row - 1, NextColumn is Colum
 % Prints the contents of a list
 printList([H | T]) :- write(H), write('\n'), printList(T).
 
-% printCell(Maze, List, Row, Column) - helper goal for printMaze, printCell
-%   prints a single cell in the maze.
-%
-%   Print a barrier.
+% Print a barrier.
 printCell(Maze, _, Row, Column) :- maze(Maze, Row, Column, barrier), write('x').
-%   Print an open cell.
+% Print an open cell.
 printCell(Maze, _, Row, Column) :- maze(Maze, Row, Column, open), write(' ').
+% Print an edge
+printCell(Maze, _, Row, Column) :- mazeSize(Maze, X, Y), ((Y == Column) ; (Column == 0)), put('|').
 
 % Print the top or bottom walls of a Maze
-printTop(Maze) :- put('+'), mazeSize(small, X, Y), \+ printDashes(Y), put('+').
+printTop(Maze) :- put('+'), mazeSize(Maze, X, Y), \+ printDashes(Y), put('+').
 printDashes(X) :- dif(X, 0), Y is X - 1, write('-'), printDashes(Y).
 
-printMaze(Maze, [H | T]) :- printTop(Maze), /* TODO printRows */, printTop(Maze).
+% Print a row of cells
+% Cols defines the number of columns left to print in the row
+printRow(Maze, _, Row, Column, Cols) :- 
+    dif(Cols, 0), printCell(Maze, _, Row, Column), 
+    X is Column + 1, Y is Cols - 1, printRow(Maze, _, Row, X, Y).
+% Print a row of a maze
+printRow(Maze, Row, [H | T]) :- mazeSize(Maze, _, Cols), Y is Cols + 2, printRow(Maze, _, Row, 0, Y).
+
+printMaze(Maze, [H | T]) :- printTop(Maze), /* TODO printRows ,*/ printTop(Maze).
 
 % -------------------------- Solving Rules --------------------------- %
 
